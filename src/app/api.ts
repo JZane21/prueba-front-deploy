04e2e14b20a2dd1b3f0c6b1e211ef964e3c9894c
Jose Carrasco <jsce2021@gmail.com>
2023-09-26 20:38:43 -0400

    next commit before deploy

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = process.env.REACT_APP_URL;
 export const baseApi = axios.create({
   baseURL: BASE_URL,
     timeout: 10000,
