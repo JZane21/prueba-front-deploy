404e498fbd9ddefb416a991851ec6c9c90ce3847
Jose Carrasco <jsce2021@gmail.com>
2023-09-26 22:05:54 -0400

    arreglando la api

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_REACT_APP_URL;
+console.log(BASE_URL);
 export const baseApi = axios.create({
   baseURL: BASE_URL,
     timeout: 10000,
--- src/app/service.ts
+++ src/app/service.ts
@@ -2,7 +2,7 @@ import { baseApi } from "./api";
 
 export const getHelloFromApi = async () => {
   try {
-    const dataFromPromise = await baseApi.get("");
+    const dataFromPromise = await baseApi.get("/");
     return dataFromPromise.data;
   } catch (error) {
     console.error(error);
